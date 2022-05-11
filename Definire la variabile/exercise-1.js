ed9654189b5f8acb9107a214a47fa76ec99965d1
Tiziano Forte <forte.tiziano@gmail.com>
2022-05-11 13:11:41 +0200

    1th exercise

---
+++ Definire la variabile/exercise-1.js
@@ -0,0 +1,14 @@
+function canPlay() {
+  if (true) {
+    let personName = 'Paul';
+
+
+ personName += ' plays football'; // Argh! personName is not defined
+
+  console.log(personName);
+}}
+
+
+canPlay();
+
+//Risolto essendo block-scoped devono restare all'interno dello "scope" ovvero all'interno delle parantesi {}.
