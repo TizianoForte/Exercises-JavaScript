12cddf02dec097253631e28bafd0721a77c70e45
Tiziano Forte <forte.tiziano@gmail.com>
2022-05-19 13:04:23 +0200

    3th exercise

---
+++ 03 - Variabili e readonly/exercise-3.js
@@ -0,0 +1,10 @@
+const students = ['Paul', 'George', 'Lucas'];
+
+function addStudent(student) {
+  students.push(student)
+}
+
+addStudent('Marco');
+console.log(students);
+
+// Utilizzato la funzione Push() per aggiungere Marco al print finale 
