5575411fbe0f53c1e1fd1b4f06e6a01d9f6d3f65
Tiziano Forte <forte.tiziano@gmail.com>
2022-05-25 14:42:26 +0200

    9th exercise

---
+++ 09 - Blocchi condizionali e cicli/exercise-9.js
@@ -0,0 +1,20 @@
+function getKeys(obj) {
+  let allKeys=[];
+  for(let keypersons in obj){
+    if (Object.keys({})){
+      allKeys.push(keypersons);
+    }
+    }
+  return allKeys;
+}
+
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 32,
+  city: 'Rome',
+  job: 'Developer',
+};
+
+const keys = getKeys(person);
+console.log(keys);
