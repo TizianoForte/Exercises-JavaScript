4afe33a2d6d1800d7be5d1c10079409c40756cfe
Tiziano Forte <forte.tiziano@gmail.com>
2022-05-27 12:53:21 +0200

    15th exercise

---
+++ 15 - Closures/exercise-15.js
@@ -0,0 +1,23 @@
+function pow(base) {
+  return function(esponente) {
+    return Math.pow(base, esponente);
+  }
+}
+
+
+
+
+const pow2 = pow(2);
+const pow5 = pow(5);
+
+console.log('--- Power of 2 ---');
+console.log(pow2(4));
+console.log(pow2(2));
+console.log(pow2(8));
+
+console.log('--- Power of 5 ---');
+console.log(pow5(4));
+console.log(pow5(2));
+console.log(pow5(8));
+
+//output:risultati dovrebbero essere esatti, calcolati tramite calcolatrice.
