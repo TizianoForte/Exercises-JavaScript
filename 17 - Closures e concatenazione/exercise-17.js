3237678964ee95b6fd3444158aa662f142ac6aa1
Tiziano Forte <forte.tiziano@gmail.com>
2022-06-01 13:12:16 +0200

    17th exercise

---
+++ 17 - Closures e concatenazione/exercise-17.js
@@ -0,0 +1,33 @@
+function calculate() {
+  var result = 0;
+  const operations = {
+    add : function(num) {
+      result += num;
+      return this;
+    },
+    multiply : function(num) {  
+      result *= num;
+      return this;
+    },
+    sub: function(num) {  
+      result -= num;
+      return this;
+    },
+    divide: function(num) {
+      result /= num;
+      return this;
+    },
+    printResult: function() {
+      console.log(result);    
+    },
+
+  }
+return operations;
+}
+
+
+
+const calculator = calculate();
+calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
+
+// Output: confermato in 7;
