5e33186a921618fbca10af7a4bc7f38fc97c3d37
Tiziano Forte <forte.tiziano@gmail.com>
2022-06-03 10:32:23 +0200

    24th exercise

---
+++ 24 - Object methods/exercise-24.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.values
+
+console.log(Object.values(person)); // Posso semplicemente usare questo metodo per stampare i valori ?
+
+
