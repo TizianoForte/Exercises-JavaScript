1469f53437d6c5b6b950d83b0829cc3299dcb2bd
Tiziano Forte <forte.tiziano@gmail.com>
2022-06-16 10:31:34 +0200

    48th exercise

---
+++ 48 - Destructuring assignment/exercise-48.js
@@ -0,0 +1,14 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+
+
+const { id,firstName: name, lastName: surname,age: old } = person;
+
+console.log(id, name, surname, old);
+
+//Output: dovrebbe essere corretto.
