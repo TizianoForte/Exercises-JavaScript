fffec2e89a108b6f8d62cb33f596f3d53510be1b
Tiziano Forte <forte.tiziano@gmail.com>
2022-06-17 10:42:45 +0200

    56th exercise

---
+++ 56 - Rest parameters and spread syntax/exercise-56.js
@@ -0,0 +1,8 @@
+function sum(...numbers) {
+  return numbers.reduce((a, b) => a + b);
+}
+
+const numbers = [1, 2, 3,];
+console.log(sum(...numbers));
+
+//dovrebbe funzionare conn un numero illimitato di valori.
